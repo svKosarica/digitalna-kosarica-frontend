707363c7fbe6758a138cd52053f787cb7e0609be
svenahac <ahacsh@gmail.com>
2026-04-06 18:56:44 +0200

    fix

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { CartProvider } from "@/lib/cart";
 import { Header } from "@/components/shared/Header";
 
@@ -9,7 +10,9 @@ export default function MainLayout({
   return (
     <CartProvider>
       <div className="min-h-screen flex flex-col">
-        <Header />
+        <Suspense>
+          <Header />
+        </Suspense>
         <main className="flex-1 bg-background">{children}</main>
 
         <footer className="bg-sidebar py-8 px-6 border-t border-border/20">
@@ -19,15 +22,12 @@ export default function MainLayout({
                 Digitalna Košarica
               </p>
               <p className="text-xs uppercase tracking-wider">
-                &copy; {new Date().getFullYear()} Digitalna Košarica. Vse pravice
-                pridržane.
+                &copy; {new Date().getFullYear()} Digitalna Košarica. Vse
+                pravice pridržane.
               </p>
             </div>
             <div className="flex space-x-6 text-xs uppercase tracking-wider font-semibold">
-              <a
-                href="#"
-                className="hover:text-primary transition-colors"
-              >
+              <a href="#" className="hover:text-primary transition-colors">
                 Kontakt
               </a>
             </div>
